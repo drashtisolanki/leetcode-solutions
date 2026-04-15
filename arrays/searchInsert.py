94d09d001471050614c8a613772d3afd0c43867a
Drashti Solanki <drashtisolanki8782@gmail.com>
2026-04-15 20:34:28 +0530

    Add searchInsert function for binary search
    Implement binary search to find the insert position for a target in a sorted array.

---
+++ arrays/searchInsert.py
@@ -0,0 +1,22 @@
+"""
+LeetCode 35 - Search Insert Position
+Difficulty: Easy
+Topic: Arrays, Binary Search
+"""
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
